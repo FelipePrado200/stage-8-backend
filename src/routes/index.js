50924af63cefc6babf49f25454c85509fbcc5786
FelipePrado200 <129217143+FelipePrado200@users.noreply.github.com>
2023-10-04 23:22:47 -0300

    Add files via upload
    não coloquei o nodemodules aqui pois são muitos arquivos

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const { Router } = require('express')
+
+const usersRoutes = require('./users.routes')
+const notesRouter = require('./notes.routes')
+
+const routes = Router()
+routes.use('/users', usersRoutes)
+routes.use('/notes', notesRouter)
+
+module.exports = routes
